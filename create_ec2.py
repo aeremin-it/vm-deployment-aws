0f72139207d35e076ba5b4be8dae2f0010c2238d
aeremin-it <83626498+aeremin-it@users.noreply.github.com>
2021-05-09 22:07:05 -0500

    Fix errors

--- create_ec2.py
+++ create_ec2.py
@@ -64,7 +64,7 @@ ami_response = ec2.describe_images(
 ami_sorted = sorted(ami_response['Images'],
               key=lambda x: x['CreationDate'],
               reverse=True)
-print('Your Image ID depends on the configuration file - 'ami_sorted[0]['ImageId'])
+print('Your Image ID depends on the configuration file - ' + ami_sorted[0]['ImageId'])
 
 user_data = str('#!/bin/bash\nyum update -y\nmkdir data\nmkfs -t xfs /dev/xvdf\nmount /dev/xvdf /data\nchgrp -R users /data\nchmod 770 /data\nuseradd -G users user1\nuseradd -G users user2\nmkdir -p /home/user1/.ssh\necho '+yamlconf['server']['users'][0]['ssh_key']+' > /home/user1/.ssh/authorized_keys\nmkdir -p /home/user2/.ssh\necho '+yamlconf['server']['users'][1]['ssh_key']+' > /home/user2/.ssh/authorized_keys\nchmod 700 /home/user1/.ssh && chmod 600 /home/user1/.ssh/authorized_keys\nchown -R user1:user1 /home/user1/.ssh\nchmod 700 /home/user2/.ssh && chmod 600 /home/user2/.ssh/authorized_keys\nchown -R user2:user2 /home/user2/.ssh')
 
@@ -120,5 +120,5 @@ iplook = ec2.describe_instances(
     created_ec2.instance_id,
     ],
 )
-print('Public DNS Name of your instance is 'iplook['Reservations'][0]['Instances'][0]['PublicDnsName'])
-print('Public IP of your instance is 'iplook['Reservations'][0]['Instances'][0]['PublicIpAddress'])
+print('Public DNS Name of your instance is ' + iplook['Reservations'][0]['Instances'][0]['PublicDnsName'])
+print('Public IP of your instance is ' + iplook['Reservations'][0]['Instances'][0]['PublicIpAddress'])
